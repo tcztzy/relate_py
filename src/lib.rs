164116b640aa7f921583177bffb3758409058f4b
Tang Ziya <tcztzy@gmail.Com>
2023-02-18 13:39:28 +0800

    UPDATE: for relate crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
-use relate::pipelines::{make_chunks as make_chunks_rs, paint as paint_rs};
+use relate::pipelines::{MakeChunks, Paint};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn make_chunks(haps: PathBuf, sample: PathBuf, map: PathBuf, output: PathBuf, dist: Option<PathBuf>, use_transitions: bool, memory: f32) -> PyResult<()> {
-    Ok(make_chunks_rs(haps, sample, map, dist.unwrap_or(PathBuf::from("unspecified")), output, !use_transitions, memory).unwrap())
+fn make_chunks(haps: PathBuf, sample: PathBuf, map: PathBuf, output: PathBuf, dist: Option<PathBuf>, use_transitions: Option<bool>, memory: Option<f32>) -> PyResult<()> {
+    let options = MakeChunks::new(
+        haps, sample, map, dist.unwrap_or(PathBuf::from("unspecified")), output, !use_transitions.unwrap_or(true), memory.unwrap_or(5.)
+    );
+    Ok(options.execute().unwrap())
 }
 
 #[pyfunction]
 fn paint(output: PathBuf, chunk_index: usize, theta: f64, rho: f64) -> PyResult<()> {
     let painting = vec![theta, rho];
-    Ok(paint_rs(&output, chunk_index, painting).unwrap())
+    let options = Paint::new(chunk_index, output, painting);
+    Ok(options.execute().unwrap())
 }
 
 /// A Python module implemented in Rust.
